ef63d0f6019fd116cf630a13700ebb6c3b9ae408
Claus Due <claus@namelesscoder.net>
2018-10-02 20:51:17 +0200

    [FEATURE] Declare support for TYPO3v9 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-7.2.99',
-            'typo3' => '8.7.10-9.1.99',
+            'typo3' => '8.7.10-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
